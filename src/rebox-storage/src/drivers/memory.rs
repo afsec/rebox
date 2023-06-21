59394968e41e07ce3d35ac4bd9a04205b9040096
Antonio Costa <97892233+afsec@users.noreply.github.com>
2023-06-21 17:13:50 -0300

    feat: implement basic layout for memory storage

--- src/rebox-storage/src/drivers/memory.rs
+++ src/rebox-storage/src/drivers/memory.rs
@@ -1,6 +1,11 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, io::Bytes};
+
+use anyhow::bail;
+use bytes::{Buf, BufMut, BytesMut};
 
 use rebox_types::ReboxResult;
+
+const COLUMN_MAX_CAPACITY: usize = 1024 * 1024 * 1024 * 50; // 50 MBytes
 
 #[derive(Debug)]
 struct MaxSize(usize);
@@ -62,4 +67,17 @@ pub struct Column {
 pub struct ColumnName(String);
 
 #[derive(Debug, Default)]
-pub struct ColumnStorage(BTreeMap<u32, String>);
+pub struct ColumnStorage(BTreeMap<u32, ColumnContent>);
+
+#[derive(Debug, Default)]
+pub struct ColumnContent(BytesMut);
+
+impl ColumnContent {
+    pub fn store(mut self, payload: Box<dyn Buf>) -> ReboxResult<()> {
+        if self.0.capacity() >= COLUMN_MAX_CAPACITY {
+            bail!("Out ot space inside a column")
+        }
+        self.0.put(payload);
+        Ok(())
+    }
+}
