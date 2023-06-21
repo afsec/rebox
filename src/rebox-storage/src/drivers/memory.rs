365f1c4829464d00157a77e50f3aafa854ba2362
Antonio Costa <97892233+afsec@users.noreply.github.com>
2023-06-21 16:56:13 -0300

    feat: add basic type for table and column mapping

--- src/rebox-storage/src/drivers/memory.rs
+++ src/rebox-storage/src/drivers/memory.rs
@@ -1,21 +1,27 @@
 use rebox_types::ReboxResult;
 
+#[derive(Debug)]
+struct MaxSize(usize);
+impl Default for MaxSize {
+    /// 4 GBytes
+    fn default() -> Self {
+        // Supporting from 32 bit architectures
+        Self(u32::MAX as usize)
+    }
+}
+
 #[derive(Debug, Default)]
 pub struct DriverMemory {
     master_table: MasterTable,
     tables: Vec<Table>,
+    max_size: MaxSize,
 }
 
-#[derive(Debug, Default)]
-pub struct TableName;
-
-#[derive(Debug, Default)]
-pub struct MasterTable(Vec<TableName>);
-
-#[derive(Debug, Default)]
-pub struct Table {}
-
 impl DriverMemory {
+    pub fn current_size(&self) -> usize {
+        // TODO
+        std::mem::size_of::<Self>()
+    }
     pub fn run(self) -> ReboxResult<()> {
         println!("Hello from: {} at line {}.", file!(), line!());
         Ok(())
@@ -23,4 +29,35 @@ impl DriverMemory {
 }
 
 #[derive(Debug, Default)]
-pub struct Void;
+pub struct Database {
+    master_table: MasterTable,
+    tables: Vec<Table>,
+}
+
+#[derive(Debug, Default)]
+pub struct MasterTable(Table);
+
+#[derive(Debug, Default)]
+pub struct Table {
+    table_name: TableName,
+    table_metadata_filename: TableFileName,
+    columns: Vec<Column>,
+}
+
+#[derive(Debug, Default)]
+pub struct TableName(String);
+
+#[derive(Debug, Default)]
+pub struct TableFileName(String);
+
+#[derive(Debug, Default)]
+pub struct Column {
+    column_name: ColumnName,
+    column_filename: ColumnFileName,
+}
+
+#[derive(Debug, Default)]
+pub struct ColumnName(String);
+
+#[derive(Debug, Default)]
+pub struct ColumnFileName(String);
