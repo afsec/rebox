d92b8d8ece52d07b3582d1ad1af43d34f776b4d7
Antonio Costa <97892233+afsec@users.noreply.github.com>
2023-06-30 15:18:31 -0300

    fix(xtask): xshell issue on running inside closures

--- xtask/src/tasks/prepare.rs
+++ xtask/src/tasks/prepare.rs
@@ -22,27 +22,30 @@ impl Runner for Prepare {
     fn run(&self, sh: &Shell) -> XtaskResult<()> {
         println!("{self}:\n");
 
-        const COMMANDS_TO_EXECUTE: [&str; 3] =
-            ["cargo check", "cargo build", "cargo build --release"];
-
-        let refcell_sh = RefCell::new(sh);
-        dbg!(sh.current_dir());
-
-        // TODO: xshell when inside closure it losts Nix's contexts
-        COMMANDS_TO_EXECUTE
-            .iter()
-            .try_for_each(|to_execute_str| -> XtaskResult<()> {
-                let inner_sh = *(refcell_sh.borrow());
-                // let cmd = cmd!(sh, "{to_execute_str}");
-                let cmd = cmd!(inner_sh, "{to_execute_str}");
-                if self.verbose() {
-                    cmd.run()?;
-                } else {
-                    cmd.ignore_stdout().ignore_stderr().run()?;
-                }
-                Ok(())
-            })?;
-
+        {
+            let cmd = cmd!(sh, "cargo check");
+            if self.verbose() {
+                cmd.run()?;
+            } else {
+                cmd.ignore_stdout().ignore_stderr().run()?;
+            }
+        }
+        {
+            let cmd = cmd!(sh, "cargo build");
+            if self.verbose() {
+                cmd.run()?;
+            } else {
+                cmd.ignore_stdout().ignore_stderr().run()?;
+            }
+        }
+        {
+            let cmd = cmd!(sh, "cargo build --release");
+            if self.verbose() {
+                cmd.run()?;
+            } else {
+                cmd.ignore_stdout().ignore_stderr().run()?;
+            }
+        }
         Ok(())
     }
 }
