9e4dfd6806dd96da132c953e85905f5321e1f220
Antonio Costa <97892233+afsec@users.noreply.github.com>
2023-07-21 22:08:55 -0300

    refactor: column value

--- src/rebox-types/src/schema/column/model.rs
+++ src/rebox-types/src/schema/column/model.rs
@@ -51,6 +51,28 @@ pub enum ColumnValue {
     Natural(u64),
     Text(String),
 }
+
+impl From<bool> for ColumnValue {
+    fn from(value: bool) -> Self {
+        Self::Bool(value)
+    }
+}
+impl From<i64> for ColumnValue {
+    fn from(value: i64) -> Self {
+        Self::Integer(value)
+    }
+}
+impl From<u64> for ColumnValue {
+    fn from(value: u64) -> Self {
+        Self::Natural(value)
+    }
+}
+impl From<String> for ColumnValue {
+    fn from(value: String) -> Self {
+        Self::Text(value)
+    }
+}
+
 impl Display for ColumnValue {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
